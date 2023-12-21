cf89d9049b4b826a32f3e1eee89d5927ca891598
habib-zinelabidine <habib.zinelabidine@gmail.com>
2023-12-21 23:28:54 +0100

    api

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+const Port = 3000;
+app.listen(Port, () => {
+  console.log(`Server running on port ${Port}`);
+});
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/listing", listingRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar :{
+      type: String,
+      default : "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-image-icon-default-avatar-profile-icon-social-media-user-vector-image-209162840.jpg"
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
