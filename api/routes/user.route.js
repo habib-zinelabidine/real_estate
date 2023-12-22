d6a1fe9b00b38ba77b0a8c63c79462be639337d2
habib-zinelabidine <habib.zinelabidine@gmail.com>
2023-12-22 15:48:37 +0100

    create user api get listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { deleteUser, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUserListings, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get('/listings/:id',verifyToken,getUserListings);
+
 export default router;
