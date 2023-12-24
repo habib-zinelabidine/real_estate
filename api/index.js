9ef8c727805c53737628dc296e0141b4d0e57112
habib-zinelabidine <habib.zinelabidine@gmail.com>
2023-12-24 17:12:39 +0100

    adding build config

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
+
 const app = express();
 const Port = 3000;
 app.listen(Port, () => {
@@ -25,6 +28,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
